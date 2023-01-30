f2c1c5e66886d9e1d283132151792991b00935d2
Ashutosh Dubey <ashutosh.dubey@codingninjas.com>
2023-01-31 02:22:27 +0530

    add code files

---
+++ example/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://admin.codingninjas.com/users/sign_in");
+
+		String title = driver.getTitle();
+		System.out.println(title);
+
+		driver.findElements(By.className("login_link")).get(1).click();
+
+		WebElement email = driver.findElement(By.cssSelector("input[type='email']"));
+		email.sendKeys(args[0]);
+
+		List<WebElement> findElements = driver.findElements(By.tagName("button"));
+		for (int i = 0; i < findElements.size(); i++) {
+			if (findElements.get(i).getText().equals("Next"))
+				findElements.get(i).click();
+		}
+
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+
+		WebElement pwd = driver.findElement(By.id("password")).findElement(By.tagName("input"));
+		pwd.sendKeys(args[1]);
+
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+
+		findElements = driver.findElements(By.tagName("button"));
+		for (int i = 0; i < findElements.size(); i++) {
+			if (findElements.get(i).getText().equals("Next"))
+				findElements.get(i).click();
+		}
+
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.get("https://admin.codingninjas.com/problems/new");
+
+		// driver.quit();
+
+	}
+}
